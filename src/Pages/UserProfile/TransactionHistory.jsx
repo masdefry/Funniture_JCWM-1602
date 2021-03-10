18b8efcd27796a3bd40dd3e870998de8033b4918
masdefry <muhammaddefryan@gmail.com>
2021-03-10 10:18:53 +0700

    Show Data Transaction History

--- src/Pages/UserProfile/TransactionHistory.jsx
+++ src/Pages/UserProfile/TransactionHistory.jsx
@@ -1,82 +1,102 @@
+import axios from 'axios'
 import React from 'react'
 
 export default class TransactionHistory extends React.Component{
+
+    state = {
+        dataTransaction: null
+    }
+
+    componentDidMount(){
+        this.getDataTransactions()
+    }
+
+    getDataTransactions = () => {
+        let idUSer = localStorage.getItem('id')
+
+        axios.get(`http://localhost:2000/transactions?idUser=${idUSer}`)
+        .then((res) => {
+            this.setState({dataTransaction: res.data})
+            console.log(res.data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
     render(){
+        if(this.state.dataTransaction === null){
+            return(
+                <div>
+                    Loading
+                </div>
+            )
+        }
         return(
             <div className='container mt-3'>
-                <div className="row shadow py-4 mb-3">
-                    <div className="col-4">
-                        <h5 style={{lineHeight: '5px'}}>Status :</h5>
-                        <p>Unpaid</p>
-                    </div>
-                    <div className="col-4 text-center border-left border-right">
-                        <p>
-                            3 Mar 2021 09:18 WIB
-                        </p>
-                    </div>
-                    <div className="col-4 text-right">
-                        <input type='button' value='Pay Now' className='btn btn-primary' />
-                    </div>
-                    <div className="col-2 mt-3 mb-4">
-                        {/* Image */}
-                        <img src='https://cdn-m2.fabelio.com/catalog/product/s/e/set_sofa_bran_2_1_dudukan.png?auto=format&w=1678&ixlib=react-9.0.3' width='100px' height='50px' />
-                    </div>
-                    <div className="col-6 mt-3 mb-4">
-                        {/* Detail Product */}
-                        <h6 style={{lineHeight: '5px'}}>
-                            Sofa Bran 2+1 
-                        </h6>
-                        <p>
-                            1 Item x Rp.2000.000.00
-                        </p>
-                    </div>
-                    <div className="col-4 text-right mt-3 mb-4">
-                        {/* Total Price Per-Product */}
-                        <p>
-                            Total Belanja
-                        </p>
-                        <h6>
-                            Rp.2000.000.00
-                        </h6>
-                    </div>
-                </div>
-
-                <div className="row shadow py-4 mb-3">
-                    <div className="col-4">
-                        <h5 style={{lineHeight: '5px'}}>Status :</h5>
-                        <p>Unpaid</p>
-                    </div>
-                    <div className="col-4 text-center border-left border-right">
-                        <p>
-                            3 Mar 2021 09:18 WIB
-                        </p>
-                    </div>
-                    <div className="col-4 text-right">
-                        <input type='button' value='Pay Now' className='btn btn-primary' />
-                    </div>
-                    <div className="col-2 mt-3 mb-4">
-                        {/* Image */}
-                        <img src='https://cdn-m2.fabelio.com/catalog/product/s/e/set_sofa_bran_2_1_dudukan.png?auto=format&w=1678&ixlib=react-9.0.3' width='100px' height='50px' />
-                    </div>
-                    <div className="col-6 mt-3 mb-4">
-                        {/* Detail Product */}
-                        <h6 style={{lineHeight: '5px'}}>
-                            Sofa Bran 2+1 
-                        </h6>
-                        <p>
-                            1 Item x Rp.2000.000.00
-                        </p>
-                    </div>
-                    <div className="col-4 text-right mt-3 mb-4">
-                        {/* Total Price Per-Product */}
-                        <p>
-                            Total Belanja
-                        </p>
-                        <h6>
-                            Rp.2000.000.00
-                        </h6>
-                    </div>
-                </div>
+                {
+                    this.state.dataTransaction.map((value, index) => {
+                        return(
+                            <div className="row shadow py-4 mb-3">
+                                <div className="col-4">
+                                    <h5 style={{lineHeight: '5px'}}>Status :</h5>
+                                    <p>{value.status}</p>
+                                </div>
+                                <div className="col-4 text-center border-left border-right">
+                                    <p>
+                                        INV/3102021/1
+                                    </p>
+                                    <p>
+                                        {value.cretedAt}
+                                    </p>
+                                </div>
+                                <div className="col-4 text-right">
+                                    {
+                                        value.status === 'Unpaid'?
+                                            <input type='button' value='Pay Now' className='btn btn-primary' />
+                                        :
+                                            null
+                                    }
+                                </div>
+                                {
+                                    value.detail.map((value, index) => {
+                                        return(
+                                            <>
+                                                <div className="col-2 mt-3 mb-4">
+                                                    {/* Image */}
+                                                    <img src={value.productImage} width='100px' height='50px' />
+                                                </div>
+                                                <div className="col-6 mt-3 mb-4">
+                                                    {/* Detail Product */}
+                                                    <h6 style={{lineHeight: '5px'}}>
+                                                        {
+                                                            value.productName
+                                                        }
+                                                    </h6>
+                                                    <p>
+                                                        {value.productQuantity} Item x Rp.{value.productPrice.toLocaleString('id-ID')}
+                                                    </p>
+                                                </div>
+                                                <div className="col-4 text-right mt-3 mb-4">
+                                                    {/* Total Price Per-Product */}
+                                                    <p>
+                                                        Total Belanja
+                                                    </p>
+                                                    <h6>
+                                                        Rp.{(value.productQuantity * value.productPrice).toLocaleString('id-ID')}
+                                                    </h6>
+                                                </div>
+                                                <div className='col-12 border-bottom'>
+                                                    
+                                                </div>
+                                            </>
+                                        )
+                                    })
+                                }
+                            </div>
+                        )
+                    })
+                }
             </div>
         )
     }
